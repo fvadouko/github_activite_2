eaea09eca45fa6cad9b34a259787a07b1b6305e8
adouko <fvadouko@live.fr>
2014-10-01 14:00:41 +0000

    correction pour prendre en compte l entête

--- envoyer_mail.php
+++ envoyer_mail.php
@@ -1,5 +1,12 @@
 <?php
 $destinataire = "testemail@toutestfacile.com";
-echo "Ce script envoie un mail à $destinataire";
-mail($destinataire, "test email 1", "merci pour ton tutorial");
+$expediteur   = "moi@monsite.com";
+$reponse      = $expediteur;
+echo "Ce script envoie un mail à $destinataire ".
+     "en précisant que l'expediteur est $expediteur ".
+     "et que la réponse doit être envoyée à $reponse";
+mail($destinataire,
+     "test email 1bis",
+     "merci pour ton tutorial",
+     "From: $expediteur\r\nReply-To: $reponse");
 ?>
